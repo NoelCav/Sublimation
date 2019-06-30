d23d54e77b141ec6e13a45b03eacb042d6aebe23
NoelCav <noelcaverly@msn.com>
2019-06-30 03:34:29 -0700

    First Commit! Noel says to normalize the torque.

---
+++ Sumblimation Alpha/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject target = null;
+    public float distance = 5f;
+    public float height = 2.5f;
+    private Transform player;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start() {
+        player = target.GetComponent<Transform>();
+        offset = new Vector3(player.position.x, player.position.y + height, player.position.z + distance);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X"), Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+
+    }
+        
+        //this.transform.rotation = new Quaternion(-normalizedV.x, this.transform.rotation.y, -normalizedV.z, 0);
+}
---
+++ Sumblimation Alpha/Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float maxForce = 20.0f;
+    public float jumpHeight = 4000.0f;
+    public float torque = 500f;
+    private float distToGround;
+    private Rigidbody rigid;
+    public GameObject playerCam;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Quaternion camRot = playerCam.transform.GetComponent<Transform>().rotation;
+
+        Vector3 force = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        force.Normalize();
+        force = camRot * force * maxForce;
+        force = new Vector3(force.x, 0, force.z);
+
+        if (IsGrounded())
+        {
+            force *= maxForce;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigid.AddForce(new Vector3(0, jumpHeight, 0));
+                Vector3 torqueV = new Vector3(Input.GetAxis("Vertical") * torque, 0, Input.GetAxis("Horizontal") * -torque);
+                torqueV = camRot * torqueV;
+                rigid.AddTorque(torqueV);
+            }
+
+        }
+
+
+        rigid.AddForce(force, ForceMode.Force);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
