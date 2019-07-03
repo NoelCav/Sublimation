6283334b72be56384b380a2bb6891bf22900e13b
Cybermancan <nmorriss7109@gmail.com>
2019-07-02 17:18:08 -0700

    Shatter
    Cube shatters when force is applied.

---
+++ Sumblimation Alpha/Assets/Scripts/ShatterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterController : MonoBehaviour
+{
+    public GameObject shatterPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.relativeVelocity.magnitude);
+        if (collision.relativeVelocity.magnitude > 5f) {
+            for (int i = 0; i < 100; i++)
+            {
+                Rigidbody debrisInstance = Instantiate(shatterPrefab.GetComponent<Rigidbody>(), gameObject.transform.position, gameObject.transform.rotation);
+            }
+            Destroy(gameObject);
+        }
+        
+    }
+}
